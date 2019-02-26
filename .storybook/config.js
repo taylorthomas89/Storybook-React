411eaf571587bb113d3419ac716846842eb8214a
taylorthomas89 <taylor.thomas000@gmail.com>
2019-02-26 14:36:39 -0800

    updated taskList to pull from store. updated test cases

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,6 @@ import requireContext from 'require-context.macro';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
-console.log(req);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
